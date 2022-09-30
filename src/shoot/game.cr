d57660f831d80aa220b861233986e6f3f73f8e83
Matt Swieboda <mswieboda@users.noreply.github.com>
2022-09-29 22:36:44 -0500

    adds linux desktop size fix testing

--- src/shoot/game.cr
+++ src/shoot/game.cr
@@ -5,7 +5,17 @@ module Shoot
     getter manager
 
     def initialize
-      super(title: "shoot")
+      mode = SF::VideoMode.desktop_mode
+      style = SF::Style::None
+
+      {% if flag?(:linux) %}
+        mode.width -= 50
+        mode.height -= 100
+
+        style = SF::Style::Default
+      {% end %}
+
+      super(title: "shoot", mode: mode, style: style)
 
       @stage = Stage.new(window)
     end
